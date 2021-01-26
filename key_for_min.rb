5c05b8dd46e2b4b9f7d73f460059a12367249657
Steven Yi <steven.yi@berkeley.edu>
2021-01-26 07:12:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min = nil 
   min_key = nil
   name_hash.each do |key, value|
-    if !min || (value < min) do
+    if !min || (value < min)
       min = value 
       min_key = key
     end
