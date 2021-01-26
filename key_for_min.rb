820aad0134f8fbf3559d81a8bf0d545a249b8dcb
Steven Yi <steven.yi@berkeley.edu>
2021-01-26 07:09:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = nil 
+  min_key = nil
+  name_hash.each do |key, value|
+    min = value if (!min || value < min)
+    min_key = key
+  end
+  min_key
 end
